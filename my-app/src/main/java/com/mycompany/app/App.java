9c1d0bf388f82cb3913c8340e62118d0756d529b
AlejandraSahagun <is696428@iteso.mx>
2017-10-18 15:18:58 -0500

    Checkstyle homework

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,18 @@
-package com.mycompany.app;
+package main.java.com.mycompany.app;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /** Constructor. */
+    protected App() {
+    }
+
+    /**
+     * Método que imprime "Hello World!".
+     * @param args args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
 }
