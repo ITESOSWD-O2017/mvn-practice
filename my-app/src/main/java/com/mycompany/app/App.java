25cf1ecc9e2bebe60d4946485fafd3fd9c655c65
Luis Fernando López Ruiz <luis_lopez_ruiz@me.com>
2017-10-21 14:17:42 -0500

    Primera práctica diseño de software - Arreglo errores Checkstyle Maven

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,18 @@
 package com.mycompany.app;
 
 /**
- * Hello world!
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
  *
+ * @author  Luis Fernando López Ruiz
+ * @version 1.0
+ * @since   2017-10-21
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public final class App {
+    /**
+     * Constructor.
+     */
+    private App() {
+        // Aún no construida
     }
 }
---
+++ my-app/src/main/java/com/mycompany/app/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * App es una librería utilizada para el curso de diseño de software.
+ */
+package com.mycompany.app;
