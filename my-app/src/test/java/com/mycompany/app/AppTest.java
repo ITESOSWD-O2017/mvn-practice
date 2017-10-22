56f7bf650c9cc06391e7ef9f873da6c54a50caef
DESKTOP-GJTFPDA\inqui <luisa.fl.97@gmail.com>
2017-10-21 22:20:03 -0500

    Tarea 1

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,9 +1,9 @@
 package com.mycompany.app;
 
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    private AppTest( String testName )
     {
         super( testName );
     }
