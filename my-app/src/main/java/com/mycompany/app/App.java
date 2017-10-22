794ff9296a591250fe9e92585582193fb635b1fd
SergioAceves <sergio.aceves89@gmail.com>
2017-10-22 01:31:02 -0500

    SecondCommit

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,22 @@
 package com.mycompany.app;
-
-/**
- * Hello world!
- *
+/** Comentario de documento.
+ * Comentario de documento.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public final class App {
+    /**
+     * Inserta un título en la clase descripción.
+     * Al ser el título obligatorio, si es nulo o vacío se lanzará
+     * una excepción.
+     *
+     * @see App Constructor
+     */
+    private App() {
+
+    }
+    /** Comentario de documento.
+     *  @param args - Algo
+     */
+    public static void main(final String[] args) {
+     System.out.println("Hello World!");
     }
 }
---
+++ my-app/src/main/java/com/mycompany/app/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Inserta un título en la clase descripción.
+ * Al ser el título obligatorio, si es nulo o vacío se lanzará
+ * una excepción.
+ *
+ * no se que poner
+ */
+package com.mycompany.app;
